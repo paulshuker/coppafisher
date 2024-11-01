557569ae5d7af65158ff8f4464b251d3eb9a2fe9
Paul <paul.shuker@outlook.com>
2024-11-01 17:17:03 +0000

    utils/raw.py moved to extract/ import fix

--- coppafish/pipeline/extract_run.py
+++ coppafish/pipeline/extract_run.py
@@ -3,7 +3,8 @@ import os
 import numpy as np
 from tqdm import tqdm
 
-from .. import log, utils
+from .. import log
+from ..extract import raw
 from ..setup.notebook_page import NotebookPage
 from ..utils import indexing, tiles_io, system
 
@@ -67,7 +68,7 @@ def run_extract(config: dict, nbp_file: NotebookPage, nbp_basic: NotebookPage) -
                     pbar.update()
                     continue
 
-                channel_images = utils.raw.load_image(nbp_file, nbp_basic, t=t, c=channels, r=r, use_z=nbp_basic.use_z)
+                channel_images = raw.load_image(nbp_file, nbp_basic, t=t, c=channels, r=r, use_z=nbp_basic.use_z)
                 for im, c, file_path, file_exists in zip(channel_images, channels, file_paths, files_exist):
                     if file_exists:
                         continue
