f239f8e818ed01adaab7a7786c7bc410be70a924
Paul <paul.shuker@outlook.com>
2024-11-01 16:26:47 +0000

    deleted legacy set_basic_Info function

--- coppafish/plot/__init__.py
+++ coppafish/plot/__init__.py
@@ -2,6 +2,5 @@ from .call_spots import ViewBledCodes, ViewBleedMatrix, ViewSpotColourAndCode
 from .extract import view_extracted_images
 from .filter import view_filtered_images
 from .find_spots import n_spots_grid, view_find_spots
-from .raw import view_raw
 from .register import view_registered_images
 from .results_viewer.base_new import Viewer
