ba9ad05b441a554e4f89ffa957057fd94c6cba32
Paul <paul.shuker@outlook.com>
2025-01-27 11:01:08 +0000

    [pciseq] stricter `export_to_pciseq` unit tests

--- coppafisher/results/test/test_results_pciseq.py
+++ coppafisher/results/test/test_results_pciseq.py
@@ -1,9 +1,11 @@
 from os import path
 
+import pandas as pd
 import pytest
 
-from ...setup.notebook import Notebook
-from .. import pciseq
+from coppafisher.omp import base
+from coppafisher.results import pciseq
+from coppafisher.setup.notebook import Notebook
 
 
 @pytest.mark.notebook
@@ -20,13 +22,31 @@ def test_export_to_pciseq() -> None:
         assert path.isfile(csv_file_path)
         assert method in csv_file_path
         assert csv_file_path.endswith(".csv")
+
+        n_expected_genes = (
+            base.get_all_gene_no(nb.basic_info, nb.omp)[0].size if method == "omp" else nb.ref_spots.tile.size
+        )
+        results = pd.read_csv(csv_file_path)
+        assert len(results) == n_expected_genes
+
         csv_file_path = pciseq.export_to_pciseq(nb, method, 0.5)
         assert type(csv_file_path) is str
         assert path.isfile(csv_file_path)
         assert method in csv_file_path
-        assert csv_file_path.endswith(".csv")
+        assert csv_file_path.endswith(f"pciseq_{method}.csv")
+
+        if method == "prob":
+            n_expected_genes = (nb.call_spots.gene_probabilities[:].max(1) >= 0.5).sum()
+        elif method == "anchor":
+            n_expected_genes = (nb.call_spots.dot_product_gene_score[:] >= 0.5).sum()
+        elif method == "omp":
+            n_expected_genes = (base.get_all_scores(nb.basic_info, nb.omp)[0] >= 0.5).sum()
+        results = pd.read_csv(csv_file_path)
+        assert len(results) == n_expected_genes
+        assert csv_file_path.endswith(f"pciseq_{method}.csv")
+
         csv_file_path = pciseq.export_to_pciseq(nb, method, 0.5, 0.7)
         assert type(csv_file_path) is str
         assert path.isfile(csv_file_path)
         assert method in csv_file_path
-        assert csv_file_path.endswith(".csv")
+        assert csv_file_path.endswith(f"pciseq_{method}.csv")
