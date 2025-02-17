83fdfbb37bb5df6470c8b879fe967ee2fb81e025
Paul <paul.shuker@outlook.com>
2025-02-17 14:55:28 +0000

    [extract] no message for using pre-existing extract results when extract dir has no images, closes #238

--- coppafisher/pipeline/extract_run.py
+++ coppafisher/pipeline/extract_run.py
@@ -48,7 +48,8 @@ def run_extract(config: ConfigSection, nbp_file: NotebookPage, nbp_basic: Notebo
         os.mkdir(nbp_file.extract_dir)
     # Save the earliest used coppafisher version to extract inside of the extract directory.
     version_path = os.path.join(nbp_file.extract_dir, ".version")
-    if os.path.isfile(version_path):
+    extract_dir_contains_images: bool = os.listdir(nbp_file.extract_dir) > 1
+    if os.path.isfile(version_path) and extract_dir_contains_images:
         with open(version_path, "r") as file:
             extract_version = file.readline()
         if extract_version != system.get_software_version():
