5564de9ab4c48e66099640a59c29c660fd9efd3c
Paul <paul.shuker@outlook.com>
2025-02-24 14:53:58 +0000

    [version] bump

--- coppafisher/compatibility/base.py
+++ coppafisher/compatibility/base.py
@@ -38,6 +38,7 @@ class CompatibilityTracker:
             ("1.0.6", "none"),
             ("1.1.0", "omp"),
             ("1.1.1", "none"),
+            ("1.1.2", "none"),
         )
     )
     _stage_instructions: list[tuple[str, ...]]
