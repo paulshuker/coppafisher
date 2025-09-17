fbf0956777ff78ef467dc34ede69f056be313a80
Paul <paul.shuker@outlook.com>
2025-09-17 17:05:28 +0100

    [find spots] reduced memory usage

--- coppafisher/find_spots/detect.py
+++ coppafisher/find_spots/detect.py
@@ -1,8 +1,11 @@
+import math
 from typing import List, Optional, Union
 
 import numpy as np
 import scipy
 import torch
+
+MAX_PARTITION: int = 2_000_000
 
 
 def detect_spots(
@@ -45,26 +48,48 @@ def detect_spots(
         assert radius_xy > 0
         assert radius_z > 0
 
+    def find_locations_to_keep(locations: np.ndarray[np.int16], intensity: np.ndarray[np.float32]) -> np.ndarray[bool]:
+        locations_norm = locations.copy().astype(np.float32)
+        locations_norm[:, 2] *= radius_xy / radius_z
+        kdtree = scipy.spatial.KDTree(locations_norm)
+        # Gives a list for each maxima that contains a list of indices that are nearby neighbours, including itself.
+        pairs = kdtree.query_ball_tree(kdtree, r=radius_xy)
+        keep = np.array([len(pair) == 1 for pair in pairs], bool)
+        for i, i_pairs in enumerate(pairs):
+            if keep[i_pairs].any():
+                # A near neighbour has already been kept.
+                continue
+            if (intensity[i] >= intensity[i_pairs]).all():
+                keep[i_pairs] = False
+                keep[i] = True
+        return keep
+
     # (n_spots x 3) coordinate positions of the image local maxima.
     maxima_locations = np.array(np.array(image > intensity_thresh).nonzero()).T.astype(np.int16)
     maxima_intensities = np.array(image[tuple(maxima_locations.T)])
     del image
 
-    if remove_duplicates:
-        maxima_locations_norm = maxima_locations.astype(np.float32)
-        maxima_locations_norm[:, 2] *= radius_xy / radius_z
-        kdtree = scipy.spatial.KDTree(maxima_locations_norm)
-        # Gives a list for each maxima that contains a list of indices that are nearby neighbours, including itself.
-        pairs = kdtree.query_ball_tree(kdtree, r=radius_xy)
-        keep_maxima = np.array([len(pair) == 1 for pair in pairs], bool)
-        for i, i_pairs in enumerate(pairs):
-            if keep_maxima[i_pairs].any():
-                # A near neighbour has already been kept.
-                continue
-            if (maxima_intensities[i] >= maxima_intensities[i_pairs]).all():
-                keep_maxima[i_pairs] = False
-                keep_maxima[i] = True
-        maxima_locations = maxima_locations[keep_maxima]
-        maxima_intensities = maxima_intensities[keep_maxima]
+    if not remove_duplicates:
+        return maxima_locations, maxima_intensities
+
+    # Sometimes the KDTree query gets too large and causes a memory crash.
+    # Therefore, n_spots is partitioned.
+    n_partitions = math.ceil(maxima_intensities.size / MAX_PARTITION)
+    maxima_location_partitions = np.array_split(maxima_locations, n_partitions)
+    maxima_intensity_partitions = np.array_split(maxima_intensities, n_partitions)
+
+    keep_maxima = np.zeros_like(maxima_intensities, bool)
+    index_start = 0
+    for locations_partition, maxima_intensity_partition in zip(
+        maxima_location_partitions, maxima_intensity_partitions, strict=True
+    ):
+        keep_partition = find_locations_to_keep(locations_partition, maxima_intensity_partition)
+        keep_maxima[index_start : index_start + keep_partition.size] = keep_partition
+        index_start += keep_partition.size
+    maxima_locations = maxima_locations[keep_maxima]
+    maxima_intensities = maxima_intensities[keep_maxima]
+    keep = find_locations_to_keep(maxima_locations, maxima_intensities)
+    maxima_locations = maxima_locations[keep]
+    maxima_intensities = maxima_intensities[keep]
 
     return maxima_locations, maxima_intensities
