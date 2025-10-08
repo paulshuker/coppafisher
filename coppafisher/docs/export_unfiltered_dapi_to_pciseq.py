1ef74393939fe1944e2e98ef5a8a894b678d4cb2
Paul <46575643+paulshuker@users.noreply.github.com>
2025-10-08 17:23:22 +0100

    [export] radius norm unfiltered dapi image for pciseq option added, closes #382 (#386)

--- coppafisher/docs/export_unfiltered_dapi_to_pciseq.py
+++ coppafisher/docs/export_unfiltered_dapi_to_pciseq.py
@@ -3,4 +3,4 @@ from coppafisher.results import export_pciseq_unfiltered_dapi_image
 
 nb = Notebook("/path/to/notebook")
 config_path = "/path/to/config.ini"
-export_pciseq_unfiltered_dapi_image(nb, config_path)
+export_pciseq_unfiltered_dapi_image(nb, config_path, radius_norm_file=None)
